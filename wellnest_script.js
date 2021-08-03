acd7f8741fe009d243bb6f369720792be296e836
praise <daramolapraise@gmail.com>
2021-08-02 19:08:54 -0500

    Update wellnest_script.js

--- wellnest_script.js
+++ wellnest_script.js
@@ -14,8 +14,8 @@ function filterByAvailability(listings, selected_date) {
         let availabilities = elem.get("availability_list_custom_availability").get(0, elem.get("availability_list_custom_availability").length());
         for (let el of availabilities) {
             if (!el.get("weekdays_custom_weekday_range")) return false;
-            console.log(el.get("weekdays_custom_weekday_range").get("days_list_text"));
-            console.log(days[selected_date.getDay()])
+//             console.log(el.get("weekdays_custom_weekday_range").get("days_list_text"));
+//             console.log(days[selected_date.getDay()])
             let weekdays = el.get("weekdays_custom_weekday_range").get("days_list_text").get(0, el.get("weekdays_custom_weekday_range").get("days_list_text").length());
             if (weekdays.includes(days[selected_date.getDay()])) {
                 return true;
